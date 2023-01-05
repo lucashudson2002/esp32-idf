daa3dcd9b9389c8986ee3b407862a7a26b90a217
Lucas Dias Hudson <lucashudson2002@gmail.com>
2023-01-04 23:03:45 -0300

    adding template

---
+++ template/main/main.c
@@ -0,0 +1,13 @@
+#include "sdkconfig.h"
+// inclusão das bibliotecas
+
+// declaração de variáveis
+
+// declaração de funções
+
+void app_main(void){
+    // void setup
+    while (1){
+        // void loop
+    }
+}
